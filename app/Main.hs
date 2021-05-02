4d9e181437e690e330e20faaed591afacd8b93ba
erickcan <79339358+erickcan@users.noreply.github.com>
2021-05-02 13:24:47 -0300

    created `pctElems`; removed `getPctExts`

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,20 @@
 module Main where
 
-import Control.Arrow    (Arrow (first, (&&&)))
-import Data.Functor     ((<&>))
+import Control.Arrow    (Arrow ((&&&)))
 import Data.List        (genericLength, group, sort)
 import System.Directory (listDirectory)
 import System.FilePath  (takeExtension)
 import Text.Printf      (printf)
 
 main :: IO ()
-main = printExts . getPctExts =<< listDirectory "."
+main = printExts . pctElems . filter (/="") . fmap takeExtension =<< listDirectory "."
   where
     printExts :: [(Double, String)] -> IO ()
-    printExts = mapM_ . uncurry . printf $ "%.2f %%  %s\n"
+    printExts = mapM_ . uncurry . printf $ "%6.02f %%  %s\n"
 
--- | Get percentage of file extensions
-getPctExts :: [FilePath] -> [(Double, String)]
-getPctExts = fmap takeExtension
-  <&> filter (/="")
-  <&> group . sort
-  <&> fmap (genericLength &&& head)
-  <&> id &&& genericLength
-  <&> \(ys, len) -> first ((*100) . (/len)) <$> ys
+-- | Get percentage of each element in a list
+pctElems :: (Ord a, Fractional b) => [a] -> [(b, a)]
+pctElems xs = (calcPct . genericLength &&& head) <$> group (sort xs)
+  where
+    len     = genericLength xs
+    calcPct = (*100) . (/len)
